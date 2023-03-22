b745db08120ee44e4b2d1ec90037f4f29c4d30d5
EvaYohane <79856216+AdamCSHU@users.noreply.github.com>
2023-03-22 12:46:50 +0000

    Added base files.

---
+++ SHUHealthApp/Shared/UserSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHUHealthApp.Shared
+{
+    public class UserSession
+    {
+        public string UserName { get; set; }
+
+        public string PassWord { get; set; }
+
+        public DateTime DateTimeStamp { get; set; }
+
+    }
+}
